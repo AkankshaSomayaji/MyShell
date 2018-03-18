62bec970c5bafb12eabb0668c6058dc5fc038eba
Aditi Srinivas <aditisrinivas97@gmail.com>
2018-03-18 15:35:40 +0530

    Fixed issues with tokenize

--- src/tokens.c
+++ src/tokens.c
@@ -6,27 +6,42 @@
 
 void tokenize(char * input, char *** tok, int * num_tok){
     int i;
+
     char ** tokens = (char **)malloc(sizeof(char *) * 1);
     tokens[0] = (char *)calloc(sizeof(char *), 1);
     int num_tokens = 0;
+
     char * temp = NULL;
-    temp = strtok(input," \n\t");
-    tokens[num_tokens] = (char *)realloc(tokens[num_tokens], sizeof(char)*strlen(temp));
+    temp = strtok(input, " \n\t");
+    tokens[num_tokens] = (char *)realloc(tokens[num_tokens], sizeof(char) * (strlen(temp) + 1));
     strcpy(tokens[num_tokens], temp);
-    num_tokens++;
+    tokens[num_tokens][strlen(temp)] = '\0';
+
+    num_tokens++;    
     tokens = (char **)realloc(tokens, sizeof(char *) * (num_tokens + 1));
+    
 	while((temp = strtok(NULL, " \n\t")) != NULL) {
-        tokens[num_tokens] = (char *)realloc(tokens[num_tokens], sizeof(char)*strlen(temp));
+        tokens[num_tokens] = (char *)malloc(sizeof(char) * (strlen(temp) + 1));
         strcpy(tokens[num_tokens], temp);
+        tokens[num_tokens][strlen(temp)] = '\0';
         num_tokens++;
         tokens = (char **)realloc(tokens, sizeof(char *) * (num_tokens + 1));
     }
+
     *tok = (char **)malloc(sizeof(char *) * num_tokens);
     for(i = 0; i < num_tokens; i++){
-        (*tok)[i] = (char *)realloc(tokens[i], sizeof(char)*strlen(tokens[i]));
+        (*tok)[i] = (char *)malloc(sizeof(char) * (strlen(tokens[i]) + 1));
         strcpy((*tok)[i], tokens[i]);
+        (*tok)[i][strlen(tokens[i])] = '\0';
     }
+
     *(num_tok) = num_tokens;
+
+    for(i = 0; i < num_tokens; i++){
+        free(tokens[i]);
+    }
+    free(tokens);
+    free(temp);
     return ;
 }
 
