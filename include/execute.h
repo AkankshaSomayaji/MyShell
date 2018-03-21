3fc577f3e581717a1d5d8012899e404862c4277b
Aditi Srinivas <aditisrinivas97@gmail.com>
2018-03-21 20:43:23 +0530

    Added support for change directory

--- include/execute.h
+++ include/execute.h
@@ -4,3 +4,8 @@
 execute the command
 */
 void execute(int);
+
+/*
+Change directory to the given argument
+*/
+int change_directory(char ** arg);
--- src/execute.c
+++ src/execute.c
@@ -9,6 +9,7 @@ void execute(int num)
 {
 		int i = num-1, j , status;
 		pid_t pid;
+		int ret = 0;
 
 		char **arg = (char **)malloc(sizeof(char *) * 1);
 		for(j = 0; j < cmd_table[i].num_tokens ; j++)
@@ -18,8 +19,17 @@ void execute(int num)
 			strcpy(arg[j],cmd_table[i].cmdtkns[j]);	
 		}
 		arg = (char **)realloc(arg,sizeof(char *) * (j));
-		arg[j] = (char *)0;
+		arg[j] = NULL;
 
+		if(strcmp(arg[0], "cd") == 0){
+			ret = change_directory(arg);
+			if(ret < 0){
+				perror("cd");
+			}
+			else{
+				return;
+			}
+		}
 		
 		if((pid=fork())<0){
 			perror("fork");
@@ -34,4 +44,21 @@ void execute(int num)
 		else if(pid>0){
 			pid = waitpid(pid,&status,0);		
 		}
+}
+
+int change_directory(char ** arg){
+    int ret = 0;
+
+	if (arg[1] == NULL){
+		chdir(getenv("HOME")); 
+		return 0;
+	}
+
+	else{ 
+        ret = chdir(arg[1]); 
+		if(ret < 0){
+            return -1;
+		}
+	}
+	return 0;
 }
